decb500ab047fcdf873e81af15767c96cb235a9f
Ignacio Anello <nacho@nachoe.com>
2019-08-20 22:51:13 -0300

    Se borre value.component. Se agregan componentes de navbar, register, home. Se crea service de auth

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    // tslint:disable-next-line: comment-format
+    //Siempre hay que suscribirse a un Observable.
+    this.authService.login(this.model).subscribe(
+      response => {
+        console.log('Logged in successfully!!');
+        console.log(localStorage.getItem('token'));
+      },
+      error => {
+        return console.log('Failed to login: ', { error });
+      }
+    );
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token; //Retorna true o false
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('token: ' + localStorage.getItem('token'));
+    console.log('Logged Out!');
+  }
+}
+
